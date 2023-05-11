e6e064f11c8380e13377f2c2c044a46384096f55
Kurt <130383415+SpezialK71@users.noreply.github.com>
2023-05-11 16:48:44 -0500

    Add files via upload
    calendar-month (stage 1)

---
+++ calendar_month_01.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 11 16:41:07 2023
+    calendar_month_01.py
+@author: spezialk71
+"""
+
+
+
+
+# Calendar Month Program (stage 1)
+
+# init
+terminate = False
+
+# program greeting
+print('This program will display a calendar month between 1800 and 2099')
+
